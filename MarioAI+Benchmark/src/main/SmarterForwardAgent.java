fce311fb588312b8c2f912566aed0625e099ef14
Ryan Stillings <rystills@gmail.com>
2018-02-10 02:09:54 -0500

    reimplement trueJumpCounter so that agent releases jump upon achieving max height

--- MarioAI+Benchmark/src/main/SmarterForwardAgent.java
+++ MarioAI+Benchmark/src/main/SmarterForwardAgent.java
@@ -3,7 +3,7 @@ package main;
 import ch.idsia.agents.Agent;
 import ch.idsia.agents.controllers.BasicMarioAIAgent;
 import ch.idsia.benchmark.mario.engine.sprites.Mario;
-import ch.idsia.benchmark.mario.environments.Environment;
+import ch.idsia.benchmark.mario.environments.Environment; 
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,14 +16,12 @@ public class SmarterForwardAgent extends BasicMarioAIAgent implements Agent {
 	int trueJumpCounter = 0;
 	int trueSpeedCounter = 0;
 	
-	public SmarterForwardAgent()
-	{
+	public SmarterForwardAgent() {
 	    super("ForwardAgent");
 	    reset();
 	}
 	
-	public void reset()
-	{
+	public void reset() {
 	    action = new boolean[Environment.numberOfButtons];
 	    action[Mario.KEY_RIGHT] = true;
 	    action[Mario.KEY_SPEED] = true;
@@ -31,8 +29,56 @@ public class SmarterForwardAgent extends BasicMarioAIAgent implements Agent {
 	    trueSpeedCounter = 0;
 	}
 	
-	private boolean DangerOfGap(byte[][] levelScene)
-	{
+	/**
+	 * print a single row from our surroundings
+	 * @param numTilesOut: how many tiles away from mario we should display
+	 * @param row: the relative row offset we wish to display
+	 */
+	private void printSurroundingsRow(int numTilesOut, int row) {
+		System.out.print('|');
+		for (int i = numTilesOut; i > 0; --i) {
+			String curVal = Integer.toString(getReceptiveFieldCellValue(marioCenter[0] - row, marioCenter[1] - i));
+			if (curVal.length() < 3) {
+				curVal = ' ' + curVal + ' ';
+			}
+			System.out.print(curVal + "|");
+		}
+		for (int i = 0; i < numTilesOut+1; ++i) {
+			String curVal = Integer.toString(getReceptiveFieldCellValue(marioCenter[0] - row, marioCenter[1] + i));
+			if (curVal.length() < 3) {
+				curVal = ' ' + curVal + ' ';
+			}
+			System.out.print(curVal + "|");
+		}
+		System.out.print('\n');	
+	}
+	
+	/**
+	 * print our surrounds
+	 * @param numTilesOut: how many tiles away from mario we should display
+	 */
+	private void printSurroundings(int numTilesOut) {
+		for (int i = 0; i < numTilesOut*5; ++i) {
+			System.out.print('-');
+		}
+		System.out.print('\n');
+		for (int i = numTilesOut; i > 0; --i) {
+			printSurroundingsRow(numTilesOut, i);
+		}
+		for (int i = 0; i < numTilesOut+1; ++i) {
+			printSurroundingsRow(numTilesOut, -i);
+		}
+		for (int i = 0; i < numTilesOut*5; ++i) {
+			System.out.print('-');
+		}
+		System.out.print('\n');
+	}
+	
+	private void printSurroundings() {
+		printSurroundings(2);
+	}
+	
+	private boolean DangerOfGap(byte[][] levelScene) {
 	    int fromX = receptiveFieldWidth / 2;
 	    int fromY = receptiveFieldHeight / 2;
 	
@@ -102,81 +148,21 @@ public class SmarterForwardAgent extends BasicMarioAIAgent implements Agent {
 		return wallApproaching(levelScene);
 	}
 	
-	/**
-	 * print the current state of the levelScene 2dArray
-	 */
-	private void printLevel() {
-		System.out.println("levelPrintStart");
-		//System.out.println("player pos: " + marioCenter[0] + ", " + marioCenter[1]);
-		for (int i = 0; i < levelScene.length; ++i) {
-			String lineStr  ="";
-			for (int r = 0; r < levelScene[i].length; ++r) {
-				lineStr += levelScene[i][r];
-			}
-			System.out.println(lineStr);
-		}
-		System.out.println("levelPrintEnd");
-	}
-	
-	/**
-	 * print a single row from our surroundings
-	 * @param numTilesOut: how many tiles away from mario we should display
-	 * @param row: the relative row offset we wish to display
-	 */
-	private void printSurroundingsRow(int numTilesOut, int row) {
-		System.out.print('|');
-		for (int i = numTilesOut; i > 0; --i) {
-			String curVal = Integer.toString(getReceptiveFieldCellValue(marioCenter[0] - row, marioCenter[1] - i));
-			if (curVal.length() < 3) {
-				curVal = ' ' + curVal + ' ';
-			}
-			System.out.print(curVal + "|");
-		}
-		for (int i = 0; i < numTilesOut+1; ++i) {
-			String curVal = Integer.toString(getReceptiveFieldCellValue(marioCenter[0] - row, marioCenter[1] + i));
-			if (curVal.length() < 3) {
-				curVal = ' ' + curVal + ' ';
-			}
-			System.out.print(curVal + "|");
-		}
-		System.out.print('\n');	
-	}
-	
-	/**
-	 * print our surrounds
-	 * @param numTilesOut: how many tiles away from mario we should display
-	 */
-	private void printSurroundings(int numTilesOut) {
-		for (int i = 0; i < numTilesOut*5; ++i) {
-			System.out.print('-');
-		}
-		System.out.print('\n');
-		for (int i = numTilesOut; i > 0; --i) {
-			printSurroundingsRow(numTilesOut, i);
-		}
-		for (int i = 0; i < numTilesOut+1; ++i) {
-			printSurroundingsRow(numTilesOut, -i);
-		}
-		for (int i = 0; i < numTilesOut*5; ++i) {
-			System.out.print('-');
-		}
-		System.out.print('\n');
-	}
-	
-	private void printSurroundings() {
-		printSurroundings(2);
-	}
-	
 	public boolean[] getAction() {
 		//NOTE: the levelData is y,x; NOT x,y (ie. block to the right of mario is (center[0],center[1]+1), not (center[0]+1,center[1])
 		
-	    // this Agent requires observation integrated in advance.
-		//if (gapApproaching() || wallApproaching()) {
+		//if there is a wall in our way, jump as high as we can
 	    if (wallApproaching()) {
 	    	action[Mario.KEY_JUMP] = true;
+	    	++trueJumpCounter;
 	    }
 	    else {
 	    	action[Mario.KEY_JUMP] = false;
+	    	trueJumpCounter = 0;
+	    }
+	    if (trueJumpCounter > 16) {
+	        trueJumpCounter = 0;
+	        action[Mario.KEY_JUMP] = false;
 	    }
 	    
 	    //action[Mario.KEY_JUMP] = !action[Mario.KEY_JUMP];
