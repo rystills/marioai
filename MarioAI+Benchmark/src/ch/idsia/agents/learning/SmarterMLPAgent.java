a236552c48fe9c9ba7f3e805f1f760709b6efba7
Ryan Stillings <rystills@gmail.com>
2018-02-28 02:12:53 -0500

    add logic from rule-based agent as neural network inputs (untested)

--- MarioAI+Benchmark/src/ch/idsia/agents/learning/SmarterMLPAgent.java
+++ MarioAI+Benchmark/src/ch/idsia/agents/learning/SmarterMLPAgent.java
@@ -1,6 +1,7 @@
 package ch.idsia.agents.learning;
 
 import ch.idsia.agents.Agent;
+import ch.idsia.benchmark.mario.engine.sprites.Mario;
 import ch.idsia.benchmark.mario.environments.Environment;
 import ch.idsia.evolution.Evolvable;
 import ch.idsia.evolution.SmarterMLP;
@@ -48,6 +49,9 @@ public class SmarterMLPAgent implements Agent, Evolvable
     int zLevelScene = 1;
     int zLevelEnemies = 0;
 
+	//persistent variables for stateful logic
+    float prevY = 0;
+    protected int[] marioCenter;
 
     public SmarterMLPAgent()
     {
@@ -114,20 +118,115 @@ public class SmarterMLPAgent implements Agent, Evolvable
     	this.mlp.psoRecombine(this.mlp, parent1.mlp, parent2.mlp);
     }
     
+    /**
+	 * check if there is a gap immediately in front of us
+	 * @return: whether there is a gap immediately in front of us (true) or not (false)
+	 */
+    private boolean gapApproaching() {
+	    if (getReceptiveFieldCellValue(marioCenter[0] + 1, marioCenter[1] + 1) == 0) {
+	    	return true;
+	    }
+	    return false;
+	}
+	
+	/**
+	 * check if there is a wall 1 or 2 blocks in front of us
+	 * @return: whether there is a wall in front of us (true) or not (false)
+	 */
+	private boolean wallApproaching() {
+		if (getReceptiveFieldCellValue(marioCenter[0], marioCenter[1] + 1) != 0 || 
+				getReceptiveFieldCellValue(marioCenter[0], marioCenter[1] + 2) != 0) {
+	    	return true;
+	    }
+		return false;
+	}
+    
+    /**
+	 * check if there is an enemy 1 or 2 blocks in front of us
+	 * @return: whether there is an enemy in front of us (true) or not (false)
+	 */
+	private boolean enemyApproaching() {
+		for (int i = 0; i < enemiesFloatPos.length; i+=3) {
+			float ex = enemiesFloatPos[i+1];
+			//check if any enemies x vales are within a small area around mario
+			if (ex <= 48 && ex >= -16) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * check if we moved down since last frame; if so, force trueJumpCounter to 0 and stop holding jump
+	 * @return: whether or not we moved down since last frame and had trueJumpCounter > 1
+	 */
+	private boolean checkMovedDown() {
+		//ignore trueJumpCounter <=1 so we can still wallkick (as during wallslide we are slowly moving down)
+		if (marioFloatPos[1] > prevY) {
+	    	return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * check if we are about to stomp on an enemy; if so, hold jump
+	 * @return: whether we are about to stomp on an enemy (true) or not (false)
+	 */
+	private boolean checkAboutToStomp() {
+		//can't stomp an enemy while grouhnded
+		if (isMarioOnGround) {
+			return false;
+		}
+		//check if any enemies are within a tight bound of our x position (note that we ignore y position for simplicity)
+		for (int i = 0; i < enemiesFloatPos.length; i+=3) {
+			if (enemiesFloatPos[i+1] <= 16 && enemiesFloatPos[i+1] >= -8) {
+		    	return true;
+			}
+		}
+    	return false;
+	}
+    
+	public int getReceptiveFieldCellValue(int x, int y)
+	{
+	    if (x < 0 || x >= levelScene.length || y < 0 || y >= levelScene[0].length)
+	        return 0;
+
+	    return levelScene[x][y];
+	}
+	
     public boolean[] getAction()
     {
 //        double[] inputs = new double[]{probe(-1, -1, levelScene), probe(0, -1, levelScene), probe(1, -1, levelScene),
 //                              probe(-1, 0, levelScene), probe(0, 0, levelScene), probe(1, 0, levelScene),
 //                                probe(-1, 1, levelScene), probe(0, 1, levelScene), probe(1, 1, levelScene),
 //                                1};
-        double[] inputs = new double[]{probe(-1, -1, mergedObservation), probe(0, -1, mergedObservation), probe(1, -1, mergedObservation),
+       
+    	/*double[] inputs = new double[]{probe(-1, -1, mergedObservation), probe(0, -1, mergedObservation), probe(1, -1, mergedObservation),
                 probe(-1, 0, mergedObservation), probe(0, 0, mergedObservation), probe(1, 0, mergedObservation),
                 probe(-1, 1, mergedObservation), probe(0, 1, mergedObservation), probe(1, 1, mergedObservation),
-                1};
+                1};*/
+    	
+    	marioCenter = environment.getMarioReceptiveFieldCenter();
+    	
+    	double[] inputs = new double[] {
+    			gapApproaching() ? 1 : 0,
+    	    	wallApproaching() ? 1 : 0,
+    			enemyApproaching() ? 1 : 0,
+    			checkAboutToStomp() ? 1 : 0,
+    			checkMovedDown() ? 1 : 0,
+    			marioMode == 2 ? 1 : 0,
+    			isMarioOnGround ? 1 : 0,
+    			isMarioAbleToJump ? 1 : 0
+    	};
         double[] outputs = mlp.propagate(inputs);
         boolean[] action = new boolean[numberOfOutputs];
-        for (int i = 0; i < action.length; i++)
+        for (int i = 0; i < action.length; i++) {
             action[i] = outputs[i] > 0;
+        }
+        
+        //update persistent variables
+	    prevY = marioFloatPos[1];
+	    
         return action;
     }
 
